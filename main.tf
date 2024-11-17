c3d9d3fd76d42bd0b2d011c2ad88f6a3434622aa
Ravpreet Grewal <ravpreet.grewal@icsi.berkeley.edu>
2024-11-16 22:26:48 -0800

    Added load balancer

--- main.tf
+++ main.tf
@@ -46,6 +46,45 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.12.0"
+
+  load_balancer_type = "application"
+
+  name            = "blog_alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        target_id = aws_instance.blog.id
+        port      = 80
+      }
+
+    }
+  ]
+
+  http_tcp_listener = [
+    {
+      port               = module.blog_alb.target_groups.port
+      protocol           = module.blog_alb.target_groups.backend_protocol
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg"{
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
